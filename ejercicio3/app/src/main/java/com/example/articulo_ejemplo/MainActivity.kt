e3a7fb52499875550e9d08593242344b7f4a5d57
pablo <taddeiApablo23@gmail.com>
2024-10-16 02:32:53 -0300

    comenzando con el segundo ejemplo
    comenzando una nueva practica un articulo muy bueno

---
+++ ejercicio3/app/src/main/java/com/example/articulo_ejemplo/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.articulo_ejemplo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.articulo_ejemplo.ui.theme.Articulo_ejemploTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Articulo_ejemploTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+/*AQUI VOY A COMENZAR A ARMAR LOS BLOQUES DE LA UI QUE NECESITO*/
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Articulo_ejemploTheme {
+        Greeting("Android")
+    }
+}
